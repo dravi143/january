5c55453a293a1cc626dccd36399bf6e996691032
ravikumar <duddelaravikumar0@gmail.com>
2022-02-10 19:49:07 +0530

    python.py

---
+++ python.py
@@ -0,0 +1,8 @@
+ python was very easy language compare with java 
+ so this lamguage was so many people ignoring to learn 
+ so java become very popular thats why java was very  impoartant to every information 
+ technology employee so lot of peolple learn java so thats why we ignoring python 
+ so its better learn python thna java so we could learn python 
+
+ if we learn python its useful for artificial intrgilegence also so we could learn 
+ python was 
